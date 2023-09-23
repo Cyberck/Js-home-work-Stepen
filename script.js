8aa67db87c21d0a54810a6baf8e718025a9c8b32
unknown <Baxtiyorddushaev@gmail.com>
2023-09-23 13:56:17 +0500

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let num = +prompt('Write number', ''),
+    stepen = +prompt('Enter the degree', '');
+
+    let answer = 1;
+
+    for (let i = 0; i < stepen; i++) {
+      answer = answer * num;
+    };
+
+    alert(answer);
+
+
+
+
